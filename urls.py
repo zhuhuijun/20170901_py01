be628e59dc030d967504eb1e10f0b6ca705a4394
kngcbzdsn <zhuhjtcx@qq.com>
2017-09-01 13:32:30 +0800

    add22

--- urls.py
+++ urls.py
@@ -17,5 +17,5 @@ urlpatterns = patterns('',
     url(r'^blog/index/$','blog.views.index'),
     url(r'^calc/index/$','calc.views.index'),
     url(r'^add/$','calc.views.add',name='add'), 
-    url(r'^add2/(\d+)/(\d+)/$', calc.views.add2, name='add2'),
+    url(r'^add2/(\d+)/(\d+)/$', 'calc.views.add2', name='add2'),
 )
